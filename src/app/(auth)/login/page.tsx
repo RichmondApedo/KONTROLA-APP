7128266f83e821100c404e9dfb1669aee8f716be
Richmond Apedo <richmondapedo549@gmail.com>
2026-01-11 00:03:09 +0000

    add a fire emoji

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -246,7 +246,7 @@ export default function LoginPage() {
           Welcome to KONTROLA
         </CardTitle>
         <CardDescription>
-          Your Financial Freedom is here
+          Your Financial Freedom is here 🔥
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
