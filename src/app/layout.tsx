966576af20e8d311bf2600a670f3a533682eeff1
Richmond Apedo <richmondapedo549@gmail.com>
2026-03-10 11:33:18 +0000

    the app is taking long to load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,21 @@
 import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Providers } from '@/components/providers';
+import { Poppins, PT_Sans } from 'next/font/google';
+
+const fontPoppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['400', '600', '700'],
+});
+
+const fontPtSans = PT_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-pt-sans',
+  weight: ['400', '700'],
+});
 
 export const metadata: Metadata = {
   title: {
@@ -57,15 +72,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+    <html lang="en" suppressHydrationWarning className={`${fontPoppins.variable} ${fontPtSans.variable}`}>
       <body className="font-body antialiased">
         <Providers>{children}</Providers>
       </body>
